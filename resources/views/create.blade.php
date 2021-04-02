8a23b994f3d9386f8b92a041960c21d19802d054
Abdur097 <1941720094@student.polinema.ac.id>
2021-04-02 15:48:05 +0700

    hapus form id barang di create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -23,11 +23,6 @@
                     <form method="post" action="{{ route('barang.store') }}" id="myForm">
                         @csrf
                         <div class="form-group">
-                            <label for="id_barang">Id Barang</label>
-                            <input type="text" name="id_barang" class="form-control" id="id_barang" aria-describedby="id_barang" >
-                        </div>
-
-                        <div class="form-group">
                             <label for="kode_barang">Kode Barang</label>
                             <input type="kode_barang" name="kode_barang" class="form-control" id="kode_barang" aria-describedby="kode_barang" >
                         </div>
